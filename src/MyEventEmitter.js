2bf008fa8a20594fe63753ae127ad88a504c66d9
Stas <staskapica93@gmail.com>
2025-09-30 22:40:02 +0200

    add tips

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -6,48 +6,83 @@ class MyEventEmitter {
   }
 
   on(eventName, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('is not a function to match expected behavior.');
+    }
+
     if (!this.listeners[eventName]) {
       this.listeners[eventName] = [];
     }
     this.listeners[eventName].push(callback);
+
+    return this;
   }
 
   once(eventName, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('is not a function to match expected behavior.');
+    }
+
     if (!this.listeners[eventName]) {
       this.listeners[eventName] = [];
     }
 
     const wrapper = (...args) => {
       this.off(eventName, wrapper);
-
-      return callback(...args);
+      callback(...args);
     };
 
+    wrapper._original = callback; // ← вот так правильно
+
     this.listeners[eventName].push(wrapper);
+
+    return this;
   }
   off(eventName, callback) {
     if (this.listeners[eventName]) {
-      this.listeners[eventName] = this.listeners[eventName].filter(
-        (call) => call !== callback,
+      const index = this.listeners[eventName].findIndex(
+        (fn) => fn === callback || fn._original === callback,
       );
+
+      if (index !== -1) {
+        this.listeners[eventName].splice(index, 1);
+      }
+
+      if (this.listeners[eventName].length === 0) {
+        delete this.listeners[eventName];
+      }
     }
+
+    return this;
   }
   emit(eventName, ...args) {
     if (!this.listeners[eventName]) {
       return;
     }
 
-    for (const event of this.listeners[eventName]) {
+    const listeners = this.listeners[eventName].slice();
+
+    for (const event of listeners) {
       event(...args);
     }
   }
   prependListener(eventName, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('is not a function to match expected behavior.');
+    }
+
     if (!this.listeners[eventName]) {
       this.listeners[eventName] = [];
     }
     this.listeners[eventName].unshift(callback);
+
+    return this;
   }
   prependOnceListener(eventName, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('is not a function to match expected behavior.');
+    }
+
     if (!this.listeners[eventName]) {
       this.listeners[eventName] = [];
     }
@@ -58,14 +93,20 @@ class MyEventEmitter {
       return callback(...args);
     };
 
+    wrapper._original = callback;
+
     this.listeners[eventName].unshift(wrapper);
+
+    return this;
   }
   removeAllListeners(eventName) {
-    if (eventName && this.listeners[eventName]) {
-      delete this.listeners[eventName];
-    } else {
+    if (eventName === undefined) {
       this.listeners = {};
+    } else if (this.listeners[eventName]) {
+      delete this.listeners[eventName];
     }
+
+    return this;
   }
   listenerCount(eventName) {
     if (this.listeners[eventName]) {
